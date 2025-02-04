29daca0fed59ac5d26e0ed5fab9a590b9f945a06
Devothion <juliocesarespinozatrujillo@gmail.com>
2025-02-04 18:00:29 -0500

    cambios en el archivo de  database

--- database/database.js
+++ database/database.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const sequelize = new Sequelize(
   process.env.DB_NAME, // Nombre de la base de datos
   process.env.DB_USER, // Usuario
-  process.env.DB_PASSWORD, // Contraseña
+  process.env.DB_PASSWORD || "", // Contraseña
   {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
